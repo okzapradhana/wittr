0beb3f899797c2dec94fb9ac5f823236c163126b
okzapradhana <okzamahendra29@gmail.com>
2018-06-07 14:28:12 +0700

    first commit with some ES6 features implemented on some codes and adding some comments

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -2,6 +2,8 @@ import PostsView from './views/Posts';
 import ToastsView from './views/Toasts';
 import idb from 'idb';
 
+//_ in js is for declare private function
+
 export default function IndexController(container) {
   this._container = container;
   this._postsView = new PostsView(this._container);
@@ -11,12 +13,24 @@ export default function IndexController(container) {
   this._registerServiceWorker();
 }
 
-IndexController.prototype._registerServiceWorker = function() {
+IndexController.prototype._registerServiceWorker = () => {
   // TODO: register service worker
+  //check if browser supports serviceWorker
+  if (navigator.serviceWorker) {
+    //callback promise if success and if it's failed
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('Service Worker Registration success!!')
+    }).catch(() => {
+      console.log("Service Worker Registration failed")
+    })
+  } else{
+    return
+  }
+  
 };
 
 // open a connection to the server for live updates
-IndexController.prototype._openSocket = function() {
+IndexController.prototype._openSocket = function () {
   var indexController = this;
   var latestPostDate = this._postsView.getLatestPostDate();
 
@@ -35,33 +49,33 @@ IndexController.prototype._openSocket = function() {
   var ws = new WebSocket(socketUrl.href);
 
   // add listeners
-  ws.addEventListener('open', function() {
+  ws.addEventListener('open', function () {
     if (indexController._lostConnectionToast) {
       indexController._lostConnectionToast.hide();
     }
   });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
+  ws.addEventListener('message', function (event) {
+    requestAnimationFrame(function () {
       indexController._onSocketMessage(event.data);
     });
   });
 
-  ws.addEventListener('close', function() {
+  ws.addEventListener('close', function () {
     // tell the user
     if (!indexController._lostConnectionToast) {
       indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
     }
 
     // try and reconnect in 5 seconds
-    setTimeout(function() {
+    setTimeout(function () {
       indexController._openSocket();
     }, 5000);
   });
 };
 
 // called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function(data) {
+IndexController.prototype._onSocketMessage = function (data) {
   var messages = JSON.parse(data);
   this._postsView.addPosts(messages);
 };
--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,12 @@
-self.addEventListener('fetch', function(event) {
+//this method will called when new data fetched//
+
+self.addEventListener('fetch', (event) => {
+  /*
+    if the console.log changed and we hit refresh, then it will wait 
+    until we navigate the page to other page or close the page to make changes.
+    That's the service worker's life cycle. Because when we still at that page, the
+    new service worker isn't control the page otherwise they will wait until we navigate
+    or close the page  
+  */
   console.log(event.request);
 });
