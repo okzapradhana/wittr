ac863ef18dd72472edc73f5da4c33a9c30fb8409
okzapradhana <okzamahendra29@gmail.com>
2018-06-10 21:47:30 +0700

    create a custom response with html content type

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,12 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response('HELLO <b class="a-winner-is-me">World</b>', {
+      headers:{
+        'Content-type': 'text/html'
+      }
+    })
+  )
 });
